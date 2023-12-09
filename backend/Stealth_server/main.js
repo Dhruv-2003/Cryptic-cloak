8700469eefe722362f0a1413521e6eb7514e767f
Dhruv-2003 <dhruvagarwal2017@gmail.com>
2023-12-10 03:02:28 +0530

    feat: new Check Stealth method added

--- backend/Stealth_server/main.js
+++ backend/Stealth_server/main.js
@@ -58,6 +58,19 @@ const revealStealthKeyNoFile = async (
   return result;
 };
 
+const checkStealth = async (
+  spendingKey,
+  viewingKey,
+  stealthAddress,
+  ephemeralPublicKey,
+  viewTag
+) => {
+  //  "stealthereum reveal-stealth-key -k ./keyfile.json -s 0x4f0ee9771b4cd3e40ee80af8516bed32689ad904 -e 0x023ea3dfbd49d5c147c671173e3287d94caa49aba846cf3a665f5bb625ccb7da19";
+  const command = `${cliPath} check-stealth --spendingkey ${spendingKey} --viewingkey ${viewingKey} --stealthaddr ${stealthAddress} --ephemeralpub ${ephemeralPublicKey} --viewtag ${viewTag}`;
+  const result = await exectueCommandAsync(command);
+  return result;
+};
+
 const exectueCommandAsync = async (command) => {
   try {
     const { stdout, stderr } = await execAsync(command);
@@ -106,6 +119,7 @@ module.exports = {
   revealStealthKeyNoFile,
   showMetaAddress,
   exectueCommand,
+  checkStealth,
 };
 // exec(command, (error, stdout, stderr) => {
 //   if (error) {
@@ -136,6 +150,14 @@ module.exports = {
 // stealth address: 0x084c53dad73b23f7d709fdcc2edbe5caa44bccce
 // ephepmeral pubkey: 0x0391e14240e98bc771f00b5ad49f3f7ec92fd498e43f04708fd61f02fddc0931f2
 // view tag: 33
+
+// checkStealth(
+//   "0x6d2f70a47ddf455feb6a785b9787265f28897546bd1316224300aed627ef8cfc",
+//   "0xa2e9f98f845bb6a8d2db0a2a17a9d185fc97afd1b7949983ee367f9f08a5e0b7",
+//   "0x084c53dad73b23f7d709fdcc2edbe5caa44bccce",
+//   "0x0391e14240e98bc771f00b5ad49f3f7ec92fd498e43f04708fd61f02fddc0931f2",
+//   33
+// );
 
 // revealStealthKeyNoFile(
 //   "0x6d2f70a47ddf455feb6a785b9787265f28897546bd1316224300aed627ef8cfc",
