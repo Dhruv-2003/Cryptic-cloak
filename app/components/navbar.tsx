95e3f93f0c4ac6a81b0a952081649a2bfc7371d0
Architsharma7 <architsharma7911@gmail.com>
2023-12-10 03:09:08 +0530

    fix: modal added for metadata address

--- app/components/navbar.tsx
+++ app/components/navbar.tsx
@@ -1,18 +1,29 @@
-import React from 'react'
-import { ConnectButton } from '@rainbow-me/rainbowkit'
+import React from "react";
+import { ConnectButton } from "@rainbow-me/rainbowkit";
 import { useState } from "react";
 import { useAccount, usePublicClient, useWalletClient } from "wagmi";
 import sha256 from "sha256";
 import { privateKeyToAccount } from "viem/accounts";
-import { useNetwork } from 'wagmi'
+import { useNetwork } from "wagmi";
+import {
+  Modal,
+  ModalOverlay,
+  ModalContent,
+  ModalHeader,
+  ModalFooter,
+  ModalBody,
+  ModalCloseButton,
+  useDisclosure,
+  Button,
+} from "@chakra-ui/react";
 
 const Navbar = () => {
-
   const { address: account } = useAccount();
   const publicClient = usePublicClient();
   const { data: walletClient } = useWalletClient();
   const [spendingKey, setSpendingKey] = useState<string>();
   const [viewingKey, setViewingKey] = useState<string>();
+  const { isOpen, onOpen, onClose } = useDisclosure();
 
   const signAndGenerateKey = async () => {
     try {
@@ -35,6 +46,7 @@ const Navbar = () => {
 
       setSpendingKey(`0x${privateKey}`);
       setViewingKey(`0x${privateKey}`);
+      onOpen();
     } catch (error) {
       console.log(error);
     }
@@ -42,17 +54,47 @@ const Navbar = () => {
 
   return (
     <div className="w-screen bg-gradient-to-r from-white via-blue-100 to-rose-200">
-    <div className="flex mt-4 justify-between mx-6">
-      <div className="">
-        <p className="font-semibold text-2xl">ProjectName</p>
+      <div className="flex mt-4 justify-between mx-6">
+        <div className="">
+          <p className="font-semibold text-2xl">CrypticCloak</p>
+        </div>
+        <div className="flex">
+          <button
+            onClick={() => signAndGenerateKey()}
+            className="bg-white mx-3 border border-blue-500 rounded-xl px-6 py-1 text-lg text-blue-500 font-semibold"
+          >
+            Register
+          </button>
+          <ConnectButton accountStatus="address" showBalance={false} />
+        </div>
       </div>
-      <div className="flex">
-        <button onClick={() => signAndGenerateKey()} className='bg-white mx-3 border border-blue-500 rounded-xl px-6 py-1 text-lg text-blue-500 font-semibold'>Register</button>
-        <ConnectButton accountStatus="address" showBalance={false}/>
-      </div>
+      <Modal isOpen={isOpen} onClose={onClose}>
+        <ModalOverlay />
+        <ModalContent>
+          <ModalHeader>Keys Generated</ModalHeader>
+          <ModalCloseButton />
+          <ModalBody>
+            <div className="flex flex-col">
+              <div className="mt-4 flex flex-col">
+                <p className="text-md text-gray-600">
+                  Stealth Meta data Address
+                </p>
+                <p className="text-lg text-gray-800"></p>
+              </div>
+              <div className="mt-4 flex flex-col">
+                <button
+                  className="px-6 mx-auto flex justify-center py-2 bg-blue-500 text-white text-xl rounded-xl font-semibold border hover:scale-105 hover:bg-white hover:border-blue-500 hover:text-blue-500 duration-200"
+                >
+                  Register
+                </button>
+              </div>
+            </div>
+          </ModalBody>
+          <ModalFooter></ModalFooter>
+        </ModalContent>
+      </Modal>
     </div>
-  </div>
-  )
-}
+  );
+};
 
-export default Navbar
+export default Navbar;
